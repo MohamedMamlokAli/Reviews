9f294699f79c6c77e9744b43fe30f4a1457a76c2
MohamedMamlokAli <mohamedmamlokali@gmail.com>
2020-12-25 10:22:14 +0200

    Created Reviews clone folder

---
+++ src/Components/Places.js
@@ -0,0 +1,9 @@
+import React, { useReducer } from "react";
+import { data } from "./data.js";
+import { reducer } from "./reducer";
+const stateDefault = {
+  places: data,
+};
+const Places = () => {};
+
+export default Places;
